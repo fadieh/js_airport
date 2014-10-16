83c715462f0ab329e6ab3d648da7bec8eb8e0f18
Fadie Hannona <fadiehannona@gmail.com>
2014-10-16 19:49:34 +0100

    Refactored a little

--- spec/airportspec.js
+++ spec/airportspec.js
@@ -1,33 +1,30 @@
 describe('Our Javascript airport can do the following:', function(){
 
-	it('can have planes', function(){
+	beforeEach(function() {
+		plane = new Plane
 		airport = new Airport
+	})
+
+	it('can have planes', function(){
 		expect(airport.planes).toEqual([])
 	});
 
 	it('can allow planes to land', function(){
-		plane = new Plane
 		expect(airport.land(plane)).toEqual(plane)
 	});
 
 	it('should know how many planes it has', function(){
-		plane = new Plane
-		airport = new Airport
 		expect(airport.planes.length).toEqual(0)
 		airport.land(plane)
 		expect(airport.planes.length).toEqual(1)
 	});
 
 	it ('has a plane after it has landed', function(){
-		airport = new Airport
-		plane = new Plane
 		airport.land(plane)
 		expect(airport.planes).toEqual([plane])
 	});
 
 	it ('should allow planes to take off', function(){
-		airport = new Airport
-		plane = new Plane
 		airport.land(plane)
 		expect(airport.planes).toEqual([plane])
 		airport.takeOff(plane)
@@ -37,11 +34,9 @@ describe('Our Javascript airport can do the following:', function(){
 });
 
 
-// allow planes to take off
-// should know that the plane has taken off - let us know if the plane is or isn't in the airport.
-// 
-
-// isLanded instead of Landed? in javascript
-// splice method - returning an array with oneplane.
-// what does _ mean again - ask
-// 
+// to do
+// should only let a plane takeoff if it is present
+// should report if it s full
+// should report if its not full
+// tell us how many planes have landed
+// does not allow the same plane to land twice
