58f663ec3c9d095344240e2738e016fe9484d8ff
Fadie Hannona <fadiehannona@gmail.com>
2014-10-16 19:46:53 +0100

    should allow planes to take off

--- spec/airportspec.js
+++ spec/airportspec.js
@@ -25,11 +25,18 @@ describe('Our Javascript airport can do the following:', function(){
 		expect(airport.planes).toEqual([plane])
 	});
 
+	it ('should allow planes to take off', function(){
+		airport = new Airport
+		plane = new Plane
+		airport.land(plane)
+		expect(airport.planes).toEqual([plane])
+		airport.takeOff(plane)
+		expect(airport.planes).toEqual([])
+	});
+
 });
 
 
-// has a plane after it has landed
-// should know how many planes it has
 // allow planes to take off
 // should know that the plane has taken off - let us know if the plane is or isn't in the airport.
 // 
--- src/airport.js
+++ src/airport.js
@@ -13,3 +13,7 @@ Airport.prototype.land = function(plane) {
 	this.planes.push(plane)
 	return plane
 };
+
+Airport.prototype.takeOff = function(plane) {
+	this.planes.splice(-1,1)
+}
